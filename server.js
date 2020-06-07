7c5c96579feb34de9175f9c7b7c30b850d22efa8
jesteriruka <jesteriruka@gmail.com>
2020-06-07 14:59:24 -0300

    fix

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ RegisterCommand('vip', async (source, args) => {
       if (groups.length > 0) {
         if (source != 0) {
           const html = groups.map(([group,date]) => `O grupo "${group}" expira em ${date}`).join('<br>');
-          emitNet('chat:addMessage', chat.global ? -1 : source, {
+          emitNet('chat:addMessage', source, {
             template: `
             <div style="display:flex;align-items:center;justify-content:center;padding:10px;margin:5px 0;background-image: linear-gradient(to right, rgba(91, 192, 222, 1) 3%, rgba(91, 192, 222, 0) 95%);border-radius: 5px;">
               ${html}
@@ -178,7 +178,7 @@ RegisterCommand('vip', async (source, args) => {
       } else if (target != sender) {
         sendMessage(source, 'Este jogador não tem grupos', [220, 53, 69]);
       } else {
-        emitNet('chat:addMessage', chat.global ? -1 : source, {
+        emitNet('chat:addMessage', source, {
           template: `
           <div style="display:flex;align-items:center;justify-content:center;padding:10px;margin:5px 0;background-image: linear-gradient(to right, rgba(217, 83, 79, 1) 3%, rgba(217, 83, 79, 0) 95%);border-radius: 5px;">
             Você não possui nenhum vip ativo
